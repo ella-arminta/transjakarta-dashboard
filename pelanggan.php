5991f84b5b748adaafa4763a9d940162e1b0a7d2
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2024-06-07 15:37:56 +0700

    mongo

---
+++ pelanggan.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- waktu :  harian / mingguan / bulanan -->
+
+    <!-- CARDS -->
+    <!-- total pengeluaran perwaktu -->
+    <!-- total rata-rata pengeluaran perwaktu -->
+    <!-- total berapa kali ak sudah naik perwaktu -->
+    <!-- total rata-rata naik perwaktu  -->
+    <!-- total waktu yang digunakan dalam bis perwaktu -->
+    <!-- Total waktu rata-rata digunakan perwaktu -->
+
+    <!-- PIE CHART -->
+    <!-- rute yang sering digunakan perwaktu -->
+
+    <!-- BAR CHART -->
+    <!-- x: top 5 rutes favoritku --> 
+    <!-- y: rata-rata durasi perjalanan ku per 5 rute favorit -->
+
+
+    <!-- geospatial -->
+    <!-- list orang yang sering ditemui (corridor namenya sama, waktunya sama) -->
+</body>
+</html>
