03bdd79aac859d164f8477d6d9c1a171549ca4e2
felinaevangelica <c14210136@john.petra.ac.id>
2024-06-19 11:20:02 +0700

    ganti nama db mongodb

--- regular.php
+++ regular.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use MongoDB\Client;
 
 $client = new Client();
-$transjakarta = $client->pdds->transjakarta;
+$transjakarta = $client->pdds->dftransjakarta;
 
 $year = isset($_GET['year']) ? intval($_GET['year']) : 2022;
 
